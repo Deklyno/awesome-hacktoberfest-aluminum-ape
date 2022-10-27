a3c27edc08e03fd332e52f98d36fc8ed14bed0ae
GiuseppeR <giusepperuggiero03@gmail.com>
2022-10-27 11:46:25 +0200

    change_base

--- algoritmi/change_base/Python/change_base/Urtext_Mucago.py
+++
@@ -1,9 +0,0 @@
-while True:
-    try:
-        a = input("Insert a decimal value: ")
-        bin_a = bin(int(a))
-        print("Result in binary: " + str(bin_a))
-    except:
-        print("Enter a valid decimal integer.")
-
-
